408847ab3cb2bbd5474b87ebe2e29ea84382b7aa
Roshan Bhatta <roshanbha4tta4@gmail.com>
2023-01-15 12:35:12 +0545

    OTP authorization

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,66 +1,76 @@
-import React, { useRef, useState, Component } from "react";
-import { Form, Card, Button } from "react-bootstrap";
+import React, { useRef, useState } from "react";
+import { Form, Card, Button, Alert } from "react-bootstrap";
 import { auth } from "../firebase";
 import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
-// import { useAuth } from "../contexts/PhoneAuthContext";
-/*
-  - use rfc to create a new default function (ES7 React/Redux extention required by dsznajder)
-  - react-bootstrap components are imported to create the signup page  
- */
 
-export default class LogIn extends Component {
-  phoneNumberRef = useRef(); // gets user value
+export default function LogIn() {
+  const phoneRef = useRef();
 
-  onCaptchaVerify() {
-    window.recaptchaVerifier = new RecaptchaVerifier(
-      "recaptcha-container",
-      {
-        size: "normal",
-        callback: (response) => {
-          // reCAPTCHA solved, allow signInWithPhoneNumber.
-          // ...
-        },
-        "expired-callback": () => {
-          // Response expired. Ask user to solve reCAPTCHA again.
-          // ...
-        },
-      },
-      auth
-    );
-  }
-  onSignInSubmit() {
-    signInWithPhoneNumber(auth, phoneNumberRef, appVerifier)
+  const [error, setError] = useState(""); // no error by default
+  const [loading, setLoading] = useState(false);
+
+  async function onSignInSubmit(e) {
+    e.preventDefault();
+
+    setError("");
+    setLoading(true);
+
+    const phoneNumber = "+977" + phoneRef.current.value;
+    configureReCaptcha();
+    const appVerifier = window.recaptchaVerifier;
+
+    await signInWithPhoneNumber(auth, phoneNumber, appVerifier)
       .then((confirmationResult) => {
         // SMS sent. Prompt user to type the code from the message, then sign the
         // user in with confirmationResult.confirm(code).
         window.confirmationResult = confirmationResult;
+        setLoading(false);
         // ...
       })
       .catch((error) => {
         // Error; SMS not sent
+        setError("SMS not sent");
         // ...
       });
   }
-  render() {
-    return (
-      <>
-        <Card>
-          <Card.Body>
-            <h2 className="text-center mb-4"> Login</h2>
 
-            <Form noValidate>
-              <Form.Group id="email">
-                <Form.Label>Phone Number</Form.Label>
-                <Form.Control type="text" ref={phoneNumberRef} required />
-              </Form.Group>
-              <div id="recaptcha-container"></div>
-              <Button className="w-100 mt-3" type="Submit">
-                Log in
-              </Button>
-            </Form>
-          </Card.Body>
-        </Card>
-      </>
+  const configureReCaptcha = () => {
+    window.recaptchaVerifier = new RecaptchaVerifier(
+      "sign-in-button",
+      {
+        size: "invisible",
+        callback: (response) => {
+          // reCAPTCHA solved, allow signInWithPhoneNumber.
+          onSignInSubmit();
+          console.log("Recaptcha Verified");
+        },
+      },
+      auth
     );
-  }
+  };
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4"> Sign In</h2>
+
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={onSignInSubmit}>
+            <div id="sign-in-button"></div>
+            <Form.Group id="email">
+              <Form.Label>Phone Number</Form.Label>
+              <Form.Control type="tel" ref={phoneRef} />
+            </Form.Group>
+
+            <Button disabled={loading} className="w-100 mt-3" type="Submit">
+              Login In
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        Already have an account? Log In
+      </div>
+    </>
+  );
 }
