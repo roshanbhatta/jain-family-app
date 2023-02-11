44d7db329d382dfc7a10063e093e070dc2529cdd
Roshan Bhatta <roshanbha4tta4@gmail.com>
2023-02-11 12:10:08 +0545

    12:08 11/2 data retrival

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,60 +1,53 @@
 import React from "react";
 import { readAllUsers } from "../database/crudUserInfo";
-import { onValue, get, child } from "firebase/database";
-import { database } from "../firebase";
+import { onValue } from "firebase/database";
 
 export default class AllUsers extends React.Component {
-  //runs after form submit
-  handletest = (event) => {
+  constructor(props) {
+    super(props);
+    this.getValues = this.getValues.bind(this);
+    this.state={
+      userInfo:[]
+    }
+  }
 
-    //readAllUsers.onValue('child_added', (snapshot) => {const newValue = snapshot.val(); console.log(newValue)} );
-    
-    onValue(readAllUsers, (snapshot) => {
-      if (snapshot.exists) {
-        const newValue = snapshot.val();
-        // console.log(JSON.stringify(newValue));
-
-        // const obj = JSON.parse(JSON.stringify(newValue));
-        // console.log(obj.);
-
-        // let myMap = new Map(Object.entries(newValue));
-        // console.log(JSON.stringify(myMap));
-
-
-
-
-        // console.log( Object.keys(myFireBaseObj));
-
-        //if you want to get both values and keys / users etc seperately as objects
-        var keys = Object.keys(newValue );
-        var values = Object.values(newValue );
-
-        console.log(keys);
-        console.log(values[0].currentAddress);
-
-        // for (var key of Object.keys(newValue)) {
-        //     // this will give you the key & values for all properties
-        //     console.log(key + " -> " + p[key])
-        //     // .. process the data here! 
-        // }
-
-
-      } else {
-      }
-    });
-  };
-
+  getValues(){
+   onValue(readAllUsers, (snapshot) => {
+    if (snapshot.exists) {
+      const newValue = snapshot.val();
+      this.state.userInfo = Object.values(newValue);
+    }
+  });
+}
+// showUserValues=(array)=>{
+//   if(array.length > 0){
+//     return array.map(function(each){
+//        return (<tr>
+//          <td> {each.firstName} </td>
+//          <td> {each.lastName}</td>
+//          <td> {each.fatherName}</td>
+//          <td> {each.dateOfBirth}</td>
+//          <td> {each.gender}</td>
+//          <td> {each.occupation}</td>
+//          <td> {each.currentAddress}</td>
+//          <td> {each.nativeAddress} </td>
+//        </tr>)
+//      })
+//    }else{
+      
+//    }
+// }
   render() {
     return (
       <div className="container mt-5 w-50">
         <div className="row justify-content-center">
           <div className="col-12">
-            <button
+            {/* <button
               className="btn btn-primary mx-auto"
               onClick={this.handletest}
             >
               read all users
-            </button>
+            </button> */}
             <table className="table table-striped">
               <thead>
                 <tr>
@@ -69,16 +62,7 @@ export default class AllUsers extends React.Component {
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@mdo</td>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@mdo</td>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                </tr>
+
               </tbody>
             </table>
           </div>
