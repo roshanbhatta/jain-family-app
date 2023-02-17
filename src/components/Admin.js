adf256ea2a81c765f223629cdb001b3f93dc0f86
Roshan Bhatta <roshanbha4tta4@gmail.com>
2023-02-17 20:09:13 +0545

    Data retrived

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,72 +1,110 @@
 import React from "react";
 import { readAllUsers } from "../database/crudUserInfo";
-import { onValue } from "firebase/database";
+import { child, get, onValue } from "firebase/database";
 
 export default class AllUsers extends React.Component {
   constructor(props) {
     super(props);
-    this.getValues = this.getValues.bind(this);
-    this.state={
-      userInfo:[]
-    }
+    this.addAllItemsToTheTable = this.addAllItemsToTheTable.bind(this);
+  }
+  getAllDatabase = () => {
+    //retrives value from firebase
+    onValue(readAllUsers, (snapshot) => {
+      if (snapshot.exists) {
+        var users = [];
+        snapshot.forEach((childSnapshot) => {
+          users.push(childSnapshot.val());
+          // console.log(users);
+        });
+        // const newValue = snapshot.val();
+        this.addAllItemsToTheTable(users);
+      }
+    });
+    // console.log("print");
+  };
+  addItemToTable(
+    currentAddress,
+    dateOfBirth,
+    fatherName,
+    firstName,
+    gender,
+    lastName,
+    nativeAddress,
+    occupation,
+    verified
+  ) {
+    let tbody = document.getElementById("userTableBody");
+
+    let trow = document.createElement("tr");
+    let tdCurrentAddress = document.createElement("td");
+    let tdDateOfBirth = document.createElement("td");
+    let tdFatherName = document.createElement("td");
+    let tdFirstName = document.createElement("td");
+    let tdGender = document.createElement("td");
+    let tdLastName = document.createElement("td");
+    let tdNativeAddress = document.createElement("td");
+    let tdOccupation = document.createElement("td");
+    let tdVerified = document.createElement("td");
+
+    tdCurrentAddress.innerHTML = currentAddress;
+    tdDateOfBirth.innerHTML = dateOfBirth;
+    tdFatherName.innerHTML = fatherName;
+    tdFirstName.innerHTML = firstName;
+    tdGender.innerHTML = gender;
+    tdLastName.innerHTML = lastName;
+    tdNativeAddress.innerHTML = nativeAddress;
+    tdOccupation.innerHTML = occupation;
+    tdVerified.innerHTML = verified;
+
+    trow.appendChild(tdCurrentAddress);
+    trow.appendChild(tdDateOfBirth);
+    trow.appendChild(tdFatherName);
+    trow.appendChild(tdFirstName);
+    trow.appendChild(tdGender);
+    trow.appendChild(tdLastName);
+    trow.appendChild(tdNativeAddress);
+    trow.appendChild(tdOccupation);
+    trow.appendChild(tdVerified);
+
+    tbody.appendChild(trow);
   }
 
-  getValues(){
-   onValue(readAllUsers, (snapshot) => {
-    if (snapshot.exists) {
-      const newValue = snapshot.val();
-      this.state.userInfo = Object.values(newValue);
-    }
-  });
-}
-// showUserValues=(array)=>{
-//   if(array.length > 0){
-//     return array.map(function(each){
-//        return (<tr>
-//          <td> {each.firstName} </td>
-//          <td> {each.lastName}</td>
-//          <td> {each.fatherName}</td>
-//          <td> {each.dateOfBirth}</td>
-//          <td> {each.gender}</td>
-//          <td> {each.occupation}</td>
-//          <td> {each.currentAddress}</td>
-//          <td> {each.nativeAddress} </td>
-//        </tr>)
-//      })
-//    }else{
-      
-//    }
-// }
+  addAllItemsToTheTable(users) {
+    //console.log(users);
+    users.forEach((element) => {
+      this.addItemToTable(
+        element.currentAddress,
+        element.dateOfBirth,
+        element.fatherName,
+        element.firstName,
+        element.gender,
+        element.lastName,
+        element.nativeAddress,
+        element.occupation,
+        element.verified
+      );
+    });
+  }
   render() {
+    window.onload = this.getAllDatabase;
     return (
-      <div className="container mt-5 w-50">
-        <div className="row justify-content-center">
-          <div className="col-12">
-            {/* <button
-              className="btn btn-primary mx-auto"
-              onClick={this.handletest}
-            >
-              read all users
-            </button> */}
-            <table className="table table-striped">
-              <thead>
-                <tr>
-                  <th scope="col">First Name</th>
-                  <th scope="col">Last name</th>
-                  <th scope="col">Father Name</th>
-                  <th scope="col">DOB</th>
-                  <th scope="col">Gender</th>
-                  <th scope="col">Occupation</th>
-                  <th scope="col">Current Addr</th>
-                  <th scope="col">Native Addr</th>
-                </tr>
-              </thead>
-              <tbody>
-
-              </tbody>
-            </table>
-          </div>
-        </div>
+      <div className="container mt-3">
+        <table className="table table-dark">
+          <thead>
+            <tr>
+              <th scope="col">First Name</th>
+              <th scope="col">Last name</th>
+              <th scope="col">Father Name</th>
+              <th scope="col">DOB</th>
+              <th scope="col">Gender</th>
+              <th scope="col">Occupation</th>
+              <th scope="col">Current Addr</th>
+              <th scope="col">Native Addr</th>
+              <th scope="col">Validate Form</th>
+            </tr>
+          </thead>
+          <tbody id="userTableBody"></tbody>
+        </table>
       </div>
     );
   }
